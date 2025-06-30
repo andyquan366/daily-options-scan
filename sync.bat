d6b4bebe0d0d2274f4fd7b1c1cf02878ea8078c3
qintelligence366 <qintelligence366@gmail.com>
2025-06-29 21:34:22 -0400

    sync on Sun 06/29/2025 21:34:22.21

--- sync.bat
+++ sync.bat
@@ -1,7 +1,14 @@
 @echo off
 cd /d E:\Investment\daily_top_options
+
+:: 自动拉取远程代码并尝试合并
 git pull origin main --rebase
+
+:: 自动添加和提交更改
 git add .
-git commit -m "sync on %date% %time%"
+git commit -m "sync on %date% %time%" 2>nul
+
+:: 自动推送到 GitHub
 git push origin main
+
 pause
