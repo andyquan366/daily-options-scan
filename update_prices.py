4f267f1e7677f8b2de7c388847b689d42062e228
qintelligence366 <qintelligence366@gmail.com>
2025-08-30 11:01:42 -0400

    sync on Sat 08/30/2025 11:01:42.47

--- update_prices.py
+++ update_prices.py
@@ -11,6 +11,7 @@ tickers = [
     "YCON.NE",
     "YAMD.NE",
     "YPLT.NE",
+    "SOL-CAD",
     "LINK-CAD",
     "PYTH-CAD",
     "ONDO-CAD",
@@ -107,7 +108,7 @@ def write_prices_to_sheet_split(prices):
     values_list = [
         [[prices[0]], [prices[1]]],
         [[prices[2]], [prices[3]], [prices[4]], [prices[5]], [prices[6]]],
-        [[prices[7]],[prices[8]],[prices[9]],[prices[10]],[prices[11]],[prices[12]],[prices[13]],[prices[14]]]
+        [[prices[7]], [prices[8]], [prices[9]], [prices[10]], [prices[11]], [prices[12]], [prices[13]], [prices[14]]]
     ]
 
     for rng, vals in zip(ranges, values_list):
