96248a6220632815a9a672d77df85ed5512e73f9
qintelligence366 <qintelligence366@gmail.com>
2025-09-12 17:56:30 -0400

    sync on Fri 09/12/2025 17:56:30.59

--- update_prices.py
+++ update_prices.py
@@ -14,6 +14,7 @@ tickers = [
     "SOL-CAD",
     "PYTH-CAD",
     "ONDO-CAD",
+    "UMA-CAD",
     "ENA-CAD",
     "LINK-CAD",
     "RENDER-CAD",
@@ -24,10 +25,11 @@ tickers = [
 def fetch_prices(tickers):
     prices = []
 
-    # 先一次性批量获取 6 个 CoinGecko 代币
+    # 先一次性批量获取 7 个 CoinGecko 代币
     coingecko_map = {
         "PYTH-CAD": "pyth-network",
         "ONDO-CAD": "ondo-finance",
+        "UMA-CAD": "uma",
         "ENA-CAD": "ethena",
         "RENDER-CAD": "render-token",
         "JUP-CAD": "jupiter-exchange-solana",
@@ -85,11 +87,11 @@ def write_prices_to_sheet_split(prices):
 
     SPREADSHEET_ID = '1Rfs87zMtB9hyhkRiW1UGnAuNeLjQEcb_-9yRtLjRATI'
 
-    ranges = ["'ETF'!E16:E17", "'ETF'!E20:E24", "'ETF'!E40:E47"]
+    ranges = ["'ETF'!E16:E17", "'ETF'!E20:E24", "'ETF'!E40:E48"]
     values_list = [
         [[prices[0]], [prices[1]]],
         [[prices[2]], [prices[3]], [prices[4]], [prices[5]], [prices[6]]],
-        [[prices[7]], [prices[8]], [prices[9]], [prices[10]], [prices[11]], [prices[12]], [prices[13]], [prices[14]]]
+        [[prices[7]], [prices[8]], [prices[9]], [prices[10]], [prices[11]], [prices[12]], [prices[13]], [prices[14]], [prices[15]]]
     ]
 
     for rng, vals in zip(ranges, values_list):
