4f9a55694c5d2a3a066f6ceb2714ff562d8a6e52
qintelligence366 <qintelligence366@gmail.com>
2025-10-15 21:34:00 -0400

    sync on 10/15/2025 Wed 21:34:00.85

--- update_prices.py
+++ update_prices.py
@@ -74,7 +74,7 @@ def write_prices_to_sheet_split(prices):
 
     SPREADSHEET_ID = '1Rfs87zMtB9hyhkRiW1UGnAuNeLjQEcb_-9yRtLjRATI'
 
-    ranges = ["'ETF'!E19:E20", "'ETF'!E23:E27", "'ETF'!E56:E62"]
+    ranges = ["'ETF'!E19:E20", "'ETF'!E23:E27", "'ETF'!E58:E64"]
     values_list = [
         [[prices[0]], [prices[1]]],
         [[prices[2]], [prices[3]], [prices[4]], [prices[5]], [prices[6]]],
