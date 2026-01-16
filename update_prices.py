377953ddcbada5dc73dc87ea52d516c1b38a33ed
qintelligence366 <qintelligence366@gmail.com>
2026-01-15 19:24:25 -0500

    sync on 01/15/2026 Thu 19:24:25.17

--- update_prices.py
+++ update_prices.py
@@ -131,7 +131,7 @@ def write_prices_to_sheet_split(prices):
     ranges = ["'ETF'!E19:E20", "'ETF'!E41", "'ETF'!E46:E50", "'ETF'!E55:E64"]
     values_list = [
         [[prices[0]], [prices[1]]],
-        [[prices[2]], 
+        [[prices[2]]], 
         [prices[3]], [prices[4]], [prices[5]], [prices[6]]],[[prices[7]]],
         [[prices[8]], [prices[9]], [prices[10]], [prices[11]], [prices[12]], [prices[13]], [prices[14]], [prices[15]], [prices[16]], [prices[17]]]
     ]
